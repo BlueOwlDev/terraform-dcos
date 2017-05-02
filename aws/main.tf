d2c5b7046774acc208e81921f45d91785d8bbb53
Jeff Malnick <malnick@Jeffs-MacBook-Pro.local>
2017-05-01 17:16:21 -0700

    health check on master

--- aws/main.tf
+++ aws/main.tf
@@ -101,6 +101,14 @@ resource "aws_elb" "private-master-elb" {
     instance_protocol = "http"
   }
 
+	health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 2
+    target              = "HTTP:80/"
+    interval            = 5
+  }
+
   lifecycle {
     ignore_changes = ["name"]
   }
