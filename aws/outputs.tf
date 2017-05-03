8feb0cdc0577feaaa4914c44c618c610b034e43c
Jeff Malnick <malnick@ip-172-16-35-217.ec2.internal>
2017-05-03 09:46:54 -0700

    Add outputs for DNS ingest and make master ELB completely internal

--- aws/outputs.tf
+++ aws/outputs.tf
@@ -21,3 +21,16 @@ output "agent_ids" {
 output "public_agent_ids" {
   value = ["${aws_instance.public-agent.*.id}"]
 }
+
+output "master_ips" {
+  value = ["${aws_instance.master.*.private_ip}"]
+}
+
+# useful for dropping this into your own DNS
+output "master_elb_dns_name" {
+  value = "${aws_elb.private-master-elb.dns_name}"
+}
+
+output "master_elb_zone_id" {
+  value = "${aws_elb.private-master-elb.zone_id}"
+}
